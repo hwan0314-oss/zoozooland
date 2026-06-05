5c557ce17d9c1c3e27c452a248dda3ab15ec30a7
hwan0314-oss <hwan0314@gmail.com>
2026-06-06 01:50:00 +0900

    fix: debug_api 최신 토큰 테스트

--- instagram/debug_api.py
+++ instagram/debug_api.py
@@ -1,20 +1,30 @@
 import requests
 
-TOKEN = "IGAAiMvRTUimBBZAGFNSzh5RDhtNS13d2NwU01sRDBnRHhWQUwyZAEphdFJYd0tKUHB4MjZA2VnBJQmh6WEl6bmhpTElHRTR2bnB4Q2h1a3ptWXR2dG5ncnhVVjViM0pSYUdLdWZAOdzFYelF2bGZAkNktPUjNYbTBnY2t5anY3UEltMAZDZD"
+TOKEN = "EAGDt4rXqZAxEBRj5KTTIubWHNR8xNoRwMndrCNEVX5Dl4ZA8Isv0VbU0JJftUtBCIZA6MtuI1yZBReD7WQV8PS1kSHJU7w4nqwoBEXBLIrYtOSGyRKGVR63AVLUJ0oYGxPuevwWZCO7Oj1feYMUu2lSnZCnQ7ZAls7ibqiwqqbfrbQQ6VQGiKdGZBIpzutIfbQ9CsRPAUE9Qadfsen7Kx31Y2EaSraYTZCsNCOwZDZD"
+IG_ID = "17841444570925602"
 
-# Instagram 토큰으로 me 조회
-r1 = requests.get("https://graph.facebook.com/v21.0/me", params={"fields": "id,name", "access_token": TOKEN})
-print("me:", r1.json())
+# 1. 토큰 확인
+r0 = requests.get("https://graph.facebook.com/v21.0/me", params={"fields": "id,name", "access_token": TOKEN})
+print("토큰 유저:", r0.json())
 
-# Instagram 계정 ID 가져오기
-if "id" in r1.json():
-    ig_id = r1.json()["id"]
-    print(f"Instagram ID: {ig_id}")
+# 2. 직접 미디어 생성 시도
+r1 = requests.post(
+    f"https://graph.facebook.com/v21.0/{IG_ID}/media",
+    data={"image_url": "https://i.ibb.co/cXgfvv0n/photo.jpg", "caption": "test", "access_token": TOKEN},
+    timeout=30,
+)
+print("미디어 생성:", r1.json())
 
-    # 미디어 생성 시도
-    r2 = requests.post(
-        f"https://graph.facebook.com/v21.0/{ig_id}/media",
-        data={"image_url": "https://i.ibb.co/cXgfvv0n/photo.jpg", "caption": "test", "access_token": TOKEN},
+# 3. 페이지 토큰으로 시도
+r2 = requests.get(f"https://graph.facebook.com/v21.0/1184428141413290", params={"fields": "access_token", "access_token": TOKEN})
+page_data = r2.json()
+print("페이지 토큰:", page_data)
+
+if "access_token" in page_data:
+    page_token = page_data["access_token"]
+    r3 = requests.post(
+        f"https://graph.facebook.com/v21.0/{IG_ID}/media",
+        data={"image_url": "https://i.ibb.co/cXgfvv0n/photo.jpg", "caption": "test", "access_token": page_token},
         timeout=30,
     )
-    print("미디어 생성:", r2.json())
+    print("페이지 토큰으로 미디어 생성:", r3.json())
